e993d2cf4a764dc49aca31587a5431337add5fad
leandrolooqz-alt <leandrolooqz@gmail.com>
2026-09-18 13:56:55 -0300

    Delete src directory

--- src/data/initialData.ts
+++
@@ -1,7 +0,0 @@
-import { ReturnCase } from '../types';
-
-/**
- * Lista inicial de devoluções. Fica vazia de propósito: o sistema começa
- * "zerado" e cada devolução real é cadastrada pelo botão "Nova Devolução".
- */
-export const INITIAL_RETURNS: ReturnCase[] = [];
